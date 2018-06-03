6c39de1d5bd589b8d37768fb89bcd584e68f054e
Mark Kvetny <m.kvetny@outlook.com>
2018-06-03 12:51:14 +0200

    converted comments into tests

--- test/hangman/game_test.clj
+++ test/hangman/game_test.clj
@@ -2,26 +2,34 @@
   (:require [clojure.test :refer :all]
             [hangman.game :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(let [alphabet 
+      (set "abcdefghijklmnopqrstuvwxyz")]
+  
+  (deftest hide-test
+    (testing "simple"
+      (is (= (hide "hello" alphabet)
+             "-----")))
+    (testing "with special chars"
+      (is (= (hide "hello, world!" alphabet)
+             "-----, -----!"))))
+  
+  (deftest reveal-test
+    (testing "with special chars"
+      (is (= (reveal "hello, world!" alphabet '#{\e \w})
+             "-e---, w----!"))))
+  
+  (deftest guess-test
+    (testing "wrong guesses"
+      (is (= (wrong-guesses "hello" (set "tes"))
+             '#{\s \t})))
+    (testing "right guesses"
+      (is (= (right-guesses "hello" (set "tes"))
+             '#{\e}))))
+    
+  (deftest won?-test
+    (testing "player has won"
+      (is (won? "hello" alphabet (set "helo"))))
+    (testing "player has not won"
+      (is (not (won? "hello" alphabet (set "help")))))))
 
-
-(comment
-  (let [word "hello, world!"
-        alphabet (set "abcdefghijklmnopqrstuvwxyz")]
-    (hide word alphabet)) 
-  (let [word "hello, world!"
-        alphabet (set "abcdefghijklmnopqrstuvwxyz")] 
-    (reveal word alphabet '#{\e \w}))
-  (let [word "hello"
-        guesses (set "tes")]
-    (wrong-guesses word guesses))
-  (let [word "hello"
-        guesses (set "tes")]
-    (right-guesses word guesses))
-  (let [word "hello"
-        guesses (set "heloz")
-        alphabet (set "abcdefghijklmnopqrstuvwxyz")]
-    (won? word alphabet guesses))
-)
+(run-tests)
